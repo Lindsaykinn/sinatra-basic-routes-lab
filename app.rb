bd8575eac4dd6c09675b9a4a985e71474e626828
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-12-16 16:03:45 -0500

    commit tree 15f58d8b8c734e0891f050728e7063dfde16c9f8

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Lindsay"
+    end
+
+    get '/hometown' do
+        "My hometown is Macomb"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Don't Drop It'"
+    end
 end
